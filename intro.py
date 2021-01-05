f338c23e2faf07dc61a6de3ce577106a0961980a
Shana Scogin <scogins@gmail.com>
2021-01-05 16:42:46 -0500

    changed obj names and messed around with objs

--- intro.py
+++ intro.py
@@ -9,11 +9,7 @@
 # R vs python: https://www.dataquest.io/blog/python-vs-r/
 ##########################
 
-## Implement simple print functions
-print('hello')
-print(10 + 60)
-
-########## Running a simple regression
+########## Running a simple regression #######
 # from: https://towardsdatascience.com/simple-and-multiple-linear-regression-in-python-c928425168f9
 
 ## Import data
@@ -22,25 +18,22 @@ from sklearn import datasets as ds ## imports datasets from scikit-learn
 data = ds.load_boston() ## loads Boston dataset from datasets library
 
 ## Load data as pandas
-import numpy as np # I don't see this being used - where is this used
 import pandas as pd
 # define the data/predictors as the pre-set feature names
 df = pd.DataFrame(data.data, columns=data.feature_names)
 # Put the target (housing value -- MEDV) in another DataFrame
-target = pd.DataFrame(data.target, columns=["MEDV"]) # this means DV apparently
-print(target) # checking to see if it prints
+gouda = pd.DataFrame(data.target, columns=["MEDV"]) # this means DV apparently
 
 ## Run the model without a constant
 import statsmodels.api as sm
 X = df["RM"]
-y = target["MEDV"] # this just seems like an unnecessary step
+y = gouda["MEDV"] # this just seems like an unnecessary step
 cheese = sm.OLS(y, X).fit()
 predictions = cheese.predict(X) # make the predictions by the model
-print(X)
 
 ## Print out the stats
-print(cheese.summary()) ## why is this not printing
+print(cheese.summary()) # this prints now - not sure why this needs the print()
 
-
-## Print out df as test
-print(df)
+## Test without making new objects
+bri = sm.OLS(gouda["MEDV"], df["RM"]).fit() # so don't have to make the objs
+print(bri.summary()) # so this doesn't use the predictions
