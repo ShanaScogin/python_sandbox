c55d57d6e796bfcb4ff188afbe33fdd5e42cdef7
Shana Scogin <scogins@gmail.com>
2021-01-05 12:00:15 -0500

    messed around with code

--- intro.py
+++ intro.py
@@ -6,6 +6,7 @@
 # currently on scikit-learn==0.23
 # for scikit, watch: https://github.com/scikit-learn/scikit-learn/issues/19063
 # for debug, check into: https://atom.io/packages/python-debugger
+# R vs python: https://www.dataquest.io/blog/python-vs-r/
 ##########################
 
 ## Implement simple print functions
@@ -15,28 +16,31 @@ print(10 + 60)
 ########## Running a simple regression
 # from: https://towardsdatascience.com/simple-and-multiple-linear-regression-in-python-c928425168f9
 
-## importing data
+## Import data
 import statsmodels.api as sm
-from sklearn import datasets ## imports datasets from scikit-learn
-data = datasets.load_boston() ## loads Boston dataset from datasets library
+from sklearn import datasets as ds ## imports datasets from scikit-learn
+data = ds.load_boston() ## loads Boston dataset from datasets library
 
 ## Load data as pandas
-import numpy as np
+import numpy as np # I don't see this being used - where is this used
 import pandas as pd
 # define the data/predictors as the pre-set feature names
 df = pd.DataFrame(data.data, columns=data.feature_names)
 # Put the target (housing value -- MEDV) in another DataFrame
-target = pd.DataFrame(data.target, columns=["MEDV"])
+target = pd.DataFrame(data.target, columns=["MEDV"]) # this means DV apparently
+print(target) # checking to see if it prints
 
 ## Run the model without a constant
 import statsmodels.api as sm
 X = df["RM"]
-y = target["MEDV"]
-model = sm.OLS(y, X).fit()
-predictions = model.predict(X) # make the predictions by the model
+y = target["MEDV"] # this just seems like an unnecessary step
+cheese = sm.OLS(y, X).fit()
+predictions = cheese.predict(X) # make the predictions by the model
+print(X)
 
 ## Print out the stats
-model.summary() ## why is this not printing
+print(cheese.summary()) ## why is this not printing
+
 
 ## Print out df as test
 print(df)
